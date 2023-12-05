5e06b86beb02f663c655dcbabbb88e81aaf8a700
arunbabuceg <150684927+arunbabuceg@users.noreply.github.com>
2023-12-05 21:36:39 +0530

    model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { CoheroComponent } from './cohero/cohero.component';
+import { HelpsComponent } from './helps/helps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    CoheroComponent,
+    HelpsComponent
   ],
   imports: [
     BrowserModule,
